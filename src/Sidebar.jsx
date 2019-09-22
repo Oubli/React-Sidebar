9f4fc3294cdd73f0b648e0cab2dc2060803ec522
juanan <juanantonio@secmotic.com>
2019-09-22 18:42:00 +0200

    estructure of lib

---
+++ src/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Sidebar extends React.PureComponent {
+
+    render() {
+        return(
+            <p>This is a future sidebar</p>
+        )
+    }
+}
+
+export default Sidebar;
